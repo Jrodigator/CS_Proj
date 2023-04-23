13b11425ac5eae5cbb6e1c293fdfbd852b61953a
RossTheDoughnut <127311359+RossTheDoughnut@users.noreply.github.com>
2023-04-23 21:30:14 +0200

    Add files via upload

---
+++ CS_PROJ_23_ROSS_FINAL/GameFiles/main.py
@@ -0,0 +1,68 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                                                                                       # #
+#                                                                                                                                       #  #
+#    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #    #   #
+#    #  $$\   $$\ $$$$$$\ $$\    $$\ $$$$$$$$\  #  $$$$$$\ $$\   $$\ $$\    $$\  $$$$$$\  $$$$$$$\  $$$$$$$$\ $$$$$$$\   $$$$$$\   #    #    #
+#    #  $$ |  $$ |\_$$  _|$$ |   $$ |$$  _____| #  \_$$  _|$$$\  $$ |$$ |   $$ |$$  __$$\ $$  __$$\ $$  _____|$$  __$$\ $$  __$$\  #    #     #
+#    #  $$ |  $$ |  $$ |  $$ |   $$ |$$ |       #    $$ |  $$$$\ $$ |$$ |   $$ |$$ /  $$ |$$ |  $$ |$$ |      $$ |  $$ |$$ /  \__| #    #     #
+#    #  $$$$$$$$ |  $$ |  \$$\  $$  |$$$$$\     #    $$ |  $$ $$\$$ |\$$\  $$  |$$$$$$$$ |$$ |  $$ |$$$$$\    $$$$$$$  |\$$$$$$\   #    #     #
+#    #  $$  __$$ |  $$ |   \$$\$$  / $$  __|    #    $$ |  $$ \$$$$ | \$$\$$  / $$  __$$ |$$ |  $$ |$$  __|   $$  __$$<  \____$$\  #    #     #
+#    #  $$ |  $$ |  $$ |    \$$$  /  $$ |       #    $$ |  $$ |\$$$ |  \$$$  /  $$ |  $$ |$$ |  $$ |$$ |      $$ |  $$ |$$\   $$ | #    #     #
+#    #  $$ |  $$ |$$$$$$\    \$  /   $$$$$$$$\  #  $$$$$$\ $$ | \$$ |   \$  /   $$ |  $$ |$$$$$$$  |$$$$$$$$\ $$ |  $$ |\$$$$$$  | #    #     #
+#    #  \__|  \__|\______|    \_/    \________| #  \______|\__|  \__|    \_/    \__|  \__|\_______/ \________|\__|  \__| \______/  #    #     #
+#    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #    #     #
+#                                                                                                                                       #     #
+#                    .' '.            __       # # # # # # # # # # # # # # # #          __         .' '.                                #     #
+#           .        .   .           (__\_     #  JARED RODRIGUES 25983792   #        _/__)        .   .       .                        #     #
+#            .         .         . -{{_(|8)    #  ROSS                       #       (8|)_}}- .      .        .                         #     #
+#              ' .  . ' ' .  . '     (__/      #  TIAAN                      #        `\__)    '. . ' ' .  . '                          #    #
+#                                              # # # # # # # # # # # # # # # #                                                          #   #
+#                                                                                                                                       #  #
+#                                                                                                                                       # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+import menu, game, pygame, highscore
+
+start = "Menu" # "Menu" or "Game" or "leaderboard"
+
+# MENU LOOP
+
+if __name__ == "__main__" and start == "Menu":  
+    while True:  
+        instance = menu.Title_Screen()
+        instance.title_loop()  
+        
+## STRAIGHT TO GAME
+
+if __name__ == "__main__" and start == "Game":       
+  instance = game.Game(6)
+  instance.game_loop()
+    
+if __name__ == "__main__" and start == "leaderboard":       
+  instance = highscore.HighScore(500)
+  instance.score_loop
+
+
+
+
+
+
+
+## EXTRA 
+
+## SHADER CODE :
+## C++ with GLSL :
+## 
+    # void mainImage( out vec4 fragColor, in vec2 fragCoord )   ==> fragColour is pixel colour for each pixel
+    # {
+    # 	vec2 u = 10.*fragCoord/iResolution.x;           ==> normalize and scale up screen res
+        
+    #     vec2 s = vec2(1.,1.732);          ==> Hex angle for Hexagonal coordinates
+    #     vec2 a = mod(u     ,s)*2.-s;
+    #     vec2 b = mod(u+s*.5,s)*2.-s;      ==> credit to @lomateron for hex coords 
+        
+    # 	vec4 col = vec4(.5*min(dot(a,a),dot(b,b))) + 0.5;       ==> messes with colours to get yellow hive like
+    #     col.x = pow(col.x, 0.5);                              ==> effect that i want
+
+    #     fragColor = col * vec4(0.9, 0.8, 0.2, 0);             ==> switch monochrome to colour and output
+    # }
---
+++ CS_PROJ_23_ROSS_FINAL/GameFiles/menu.py
@@ -0,0 +1,360 @@
+
+import pygame
+# import sprite manager
+from utils import load_sprite
+import game
+from sys import exit
+import highscore as leaderboard
+
+#Implemented by John Cokayne 25863878 with a few lines by Jared Rodrigues
+#All background music also implemented by John Cokayne
+
+class Title_Screen:
+    #initialises all the components of the title screen
+    def __init__(self):
+        self.__init__pygame()
+        self.screen = pygame.display.set_mode((696, 528))
+        self.title_font = pygame.font.Font("Assets/minecraftfont.otf",50)
+        self.game_name = self.title_font.render('Hive Invaders',False,(85,69,15))
+        self.game_message = self.title_font.render('Press \'Space\' to Start',False,(85,69,15))
+        self.game_name_rect=self.game_name.get_rect(center=(348,132))
+        self.game_message_rect=self.game_message.get_rect(center=(348,396))
+        self.bee=pygame.image.load("Assets/Bee.png")
+        self.bee=pygame.transform.rotozoom(self.bee,0,1.5)
+        self.beerect=self.bee.get_rect(center=(348,264))   
+
+    #initialises pygame
+
+    def __init__pygame(self):
+        pygame.init()
+        pygame.display.set_caption("Protect The Hive")
+    
+    #draws the title screen using the screens components
+
+    def draw(self):
+            self.screen.fill((254, 200, 9))
+            self.screen.blit(self.game_name,self.game_name_rect)
+            self.screen.blit(self.game_message,self.game_message_rect)
+            self.screen.blit(self.bee,self.beerect)
+            pygame.display.flip()
+            #self.clock.tick(60)
+
+    #loops while the user is on the title_screen
+    def title_loop(self):
+        while True:
+
+            #checks if any background music is playing. If not, it starts playing Retro_Platforming.wav on a loop
+            if not pygame.mixer.get_busy():
+                bg_music = pygame.mixer.Sound("Assets\Retro_Platforming.wav")
+                bg_music.play(loops = -1)
+
+            #checks if the user pressed the quit button and quits if true. Also checks if they pressed the space button and jumps into the instruction screen loop if true.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                if event.type==pygame.KEYDOWN and event.key==pygame.K_SPACE:
+                    instance=Instruction_Screen()
+                    instance.instruction_loop()
+            self.draw()
+
+class Instruction_Screen:
+    
+    #intialises the components of the instruction screen
+    def __init__(self):
+        self.__init__pygame()
+        self.screen = pygame.display.set_mode((696, 528))
+        self.title_font = pygame.font.Font("Assets/minecraftfont.otf",50)
+        self.instruction_font = pygame.font.Font("Assets/minecraftfont.otf",30)
+        self.help_font = pygame.font.Font("Assets/minecraftfont.otf",25)
+        self.lines= [self.title_font.render('Hive Invaders',False,(85,69,15))]
+        self.lines+= [self.instruction_font.render('Instructions:',False,(85,69,15))]
+        self.lines+= [self.instruction_font.render('Use the arrow keys or [W][A][S][D] to move',False,(85,69,15))]
+        self.lines+= [self.instruction_font.render('[Space] to shoot',False,(85,69,15))]
+        self.lines+= [self.instruction_font.render('[P] to pause',False,(85,69,15))]
+        self.lines+= [self.instruction_font.render('[Mouse] to aim',False,(85,69,15))]
+        self.lines+= [self.title_font.render('Press [Space] to Start:',False,(85,69,15))]
+        self.rects= [self.lines[0].get_rect(center=(348,60))]
+        self.rects+=[self.lines[1].get_rect(center=(348,120))]
+        self.rects+=[self.lines[2].get_rect(center=(348,180))]
+        self.rects+=[self.lines[3].get_rect(center=(348,240))]
+        self.rects+=[self.lines[4].get_rect(center=(348,300))]
+        self.rects+=[self.lines[5].get_rect(center=(348,360))]
+        self.rects+=[self.lines[6].get_rect(center=(348,420))]
+        self.arrow=pygame.image.load("Assets/Arrow.png")
+        self.arrow=pygame.transform.rotozoom(self.arrow,0,0.15)
+        self.arrowrect=self.arrow.get_rect(center=(650,485))
+        self.arrowrectflipped=self.arrow.get_rect(center=(45,485))
+        self.help=False
+        self.jellies=[pygame.transform.rotozoom(pygame.image.load("Assets/jelly_0.png"),0,0.17)]
+        self.jellies+=[pygame.transform.rotozoom(pygame.image.load("Assets/jelly_1.png"),0,0.17)]
+        self.jellies+=[pygame.transform.rotozoom(pygame.image.load("Assets/jelly_2.png"),0,0.17)]
+        self.jellies+=[pygame.transform.rotozoom(pygame.image.load("Assets/jelly_3.png"),0,0.17)]
+        self.jellies+=[pygame.transform.rotozoom(pygame.image.load("Assets/jelly_b.png"),0,0.17)]
+        self.jellies+=[pygame.transform.rotozoom(pygame.image.load("Assets/jelly_h.png"),0,0.17)]
+        self.jellyrects=[self.jellies[0].get_rect(center=(80,120))]
+        self.jellyrects+=[self.jellies[1].get_rect(center=(80,175))]
+        self.jellyrects+=[self.jellies[2].get_rect(center=(80,230))]
+        self.jellyrects+=[self.jellies[3].get_rect(center=(80,285))]
+        self.jellyrects+=[self.jellies[4].get_rect(center=(80,340))]
+        self.jellyrects+=[self.jellies[5].get_rect(center=(80,395))]
+        self.helpText=[self.title_font.render('Jelly Powerups:',False,(85,69,15))]
+        self.helpText+=[self.help_font.render('Gives the basic single bullet at 0.5x damage',False,(85,69,15))]
+        self.helpText+=[self.help_font.render('Gives doubleshot at 1x damage',False,(85,69,15))]
+        self.helpText+=[self.help_font.render('Gives a blue bullet that does 1.5x damage',False,(85,69,15))]
+        self.helpText+=[self.help_font.render('Restores 1 heart',False,(85,69,15))]
+        self.helpText+=[self.help_font.render('Rebuilds barriers with +1 Hp',False,(85,69,15))]
+        self.helpText+=[self.help_font.render('Increases score',False,(85,69,15))]
+        self.helptextrects=[self.helpText[0].get_rect(center=(348,60))]
+        self.helptextrects+=[self.helpText[1].get_rect(midleft=(130,120))]
+        self.helptextrects+=[self.helpText[2].get_rect(midleft=(130,175))]
+        self.helptextrects+=[self.helpText[3].get_rect(midleft=(130,230))]
+        self.helptextrects+=[self.helpText[4].get_rect(midleft=(130,285))]
+        self.helptextrects+=[self.helpText[5].get_rect(midleft=(130,340))]
+        self.helptextrects+=[self.helpText[6].get_rect(midleft=(130,395))]
+
+#intialises pygame
+        
+    def __init__pygame(self):
+        pygame.init()
+        pygame.display.set_caption("Protect The Hive")
+    #draws the components of the instruction screen to the screen surface. if self.help is true, displays the page showing the jelly information
+    def draw(self):
+        if self.help:
+            self.screen.fill((254, 200, 9))
+            self.screen.blit(self.helpText[0],self.helptextrects[0])
+            for i in range(6):
+                self.screen.blit(self.helpText[i+1],self.helptextrects[i+1])
+                self.screen.blit(self.jellies[i],self.jellyrects[i])
+            self.screen.blit(pygame.transform.flip(self.arrow, True, False),self.arrowrectflipped)
+        else:
+            self.screen.fill((254, 200, 9))
+            for i in range(7):
+                self.screen.blit(self.lines[i],self.rects[i])
+            self.screen.blit(self.arrow,self.arrowrect)
+        pygame.display.flip()
+            #self.clock.tick(60)
+#loops while the user is on the instruction screen
+    def instruction_loop(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                #check if the space bar is pressed and if so, jumps into the menu loop and starts displaying the main menu
+                if event.type==pygame.KEYDOWN and event.key==pygame.K_SPACE:
+                    instance=Menu()
+                    instance.menu_loop()
+                #checks if the user clciks on the arrow at the bottom of the screen. if so, the screen starts displaying the jelly info page if its on the instruction page and vice versa
+                if event.type==pygame.MOUSEBUTTONUP:
+                    mouse_pos=pygame.mouse.get_pos()
+                    print(mouse_pos)
+                    if pygame.Rect(613,448,52,62).collidepoint(mouse_pos):
+                        self.help=True
+                    if pygame.Rect(31,448,52,62).collidepoint(mouse_pos):
+                        self.help=False
+            self.draw()
+class Menu:
+    #initialises the menu screen components
+    def __init__(self):
+        self.__init__pygame()
+        self.screen = pygame.display.set_mode((696, 528))
+        self.bg = pygame.transform.scale(load_sprite("menubackground"), (696, 528))
+        self.playimage=pygame.image.load("Assets/playbtn.png")
+        self.levelsimage=pygame.image.load("Assets/levelsbtn.png")
+        self.scoresimage=pygame.image.load("Assets/scoresbtn.png")
+        self.quitimage=pygame.image.load("Assets/quitbtn.png")
+        self.playrect=self.playimage.get_rect(topleft= (80,30))
+        self.levelsrect=self.levelsimage.get_rect(topleft= (80,110))
+        self.scoresrect=self.scoresimage.get_rect(topleft= (80,190))
+        self.quitrect=self.quitimage.get_rect(topleft= (80,270))
+        
+        # Game Clock & Speed
+        self.clock = pygame.time.Clock()
+        self.fps = 60
+
+        #intialises pygame
+
+    def __init__pygame(self):
+        pygame.init()
+        pygame.display.set_caption("Protect The Hive")
+    
+
+    #displays the components of the menu screen
+    def draw(self):
+        self.screen.fill((0, 0, 255))
+        self.screen.blit(self.bg, (0, 0))
+        self.screen.blit(self.playimage,self.playrect)
+        self.screen.blit(self.levelsimage,self.levelsrect)
+        self.screen.blit(self.scoresimage,self.scoresrect)
+        self.screen.blit(self.quitimage,self.quitrect)    
+        pygame.display.flip()
+        self.clock.tick(60)
+
+    #loops while the user is on the menu screen
+    def menu_loop(self):
+        while True:
+            #checks if any music is playing. if not, starts playing Retro_Platforming.wav
+            if not pygame.mixer.get_busy():
+                bg_music = pygame.mixer.Sound("Assets\Retro_Platforming.wav")
+                bg_music.play(loops = -1)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                #checks if the user clicked on any of the buttons on the main menu screen and if so, calls the method that will start displaying the appropriate screen
+                if event.type==pygame.MOUSEBUTTONUP:
+                    mouse_pos=pygame.mouse.get_pos()
+                    if self.playrect.collidepoint(mouse_pos):
+                        instance= game.Game()
+                        instance.game_loop()
+                    elif  self.scoresrect.collidepoint(mouse_pos):
+                        print("score")#score() or level select()
+                        instance = leaderboard.Leaderboard()
+                        instance.score_loop()
+                    elif  self.quitrect.collidepoint(mouse_pos):
+                        pygame.quit()
+                        exit()
+                    elif  self.levelsrect.collidepoint(mouse_pos):
+                        instance = Levels()
+                        instance.level_loop()
+            self.draw()
+
+class Levels:
+        #initialises the components of the level select screen
+        def __init__(self):
+
+            self.screen = pygame.display.set_mode((696, 528))
+            self.bg = pygame.transform.scale(load_sprite("levelsbg"), (696, 528))
+            self.level1=pygame.Rect(90,25,150,50)
+            self.level2=pygame.Rect(90,85,150,50)
+            self.level3=pygame.Rect(90,145,150,50)
+            self.level4=pygame.Rect(90,205,150,50)
+            self.level5=pygame.Rect(90,265,150,50)
+            self.back=pygame.Rect(90,325,150,50)
+            
+
+        
+        #Game Clock & Speed
+            self.clock = pygame.time.Clock()
+            self.fps = 60
+
+        #displays the level select screen
+        def draw(self):
+            self.screen.fill((0, 0, 255))
+            self.screen.blit(self.bg, (0, 0))
+            pygame.display.flip()
+            self.clock.tick(60)
+        #loops while the user is on the level select screen
+        def level_loop(self):
+            back=False
+            while True:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        quit()
+                    if event.type==pygame.MOUSEBUTTONUP:
+                        mouse_pos=pygame.mouse.get_pos()
+                        #checks if one of the levels is slelcted by clicking on it. if so, starts the game on that level
+                        if self.level1.collidepoint(mouse_pos):
+                            instance=game.Game(1)
+                            instance.game_loop()
+                            print("level 1")
+                        elif self.level2.collidepoint(mouse_pos):
+                            instance=game.Game(2)
+                            instance.game_loop()
+                            print("level 2")
+                        elif  self.level3.collidepoint(mouse_pos):
+                            instance=game.Game(3)
+                            instance.game_loop()
+                            print("level 3")
+                        elif  self.level4.collidepoint(mouse_pos):
+                            instance=game.Game(4)
+                            instance.game_loop()
+                            print("level 4")
+                        elif  self.level5.collidepoint(mouse_pos):
+                            instance=game.Game(5)
+                            instance.game_loop()
+                            print("level 5")
+                        #checks if the user pressed the back button. if so, takes the user back to the main menu
+                        elif  self.back.collidepoint(mouse_pos):
+                            back=True
+                if back: break
+                self.draw()
+
+'''Pretty sure this code is no longer used. keeping it in just incase it is necessary, we can easily reimplement it.
+class Help:
+        def __init__(self):
+            pygame.init()
+            pygame.display.set_caption("Protect The Hive")
+            self.screen = pygame.display.set_mode((696, 528))
+            self.bg = pygame.transform.scale(load_sprite("scoresbg"), (696, 528))
+            self.small_font = pygame.font.Font('freesansbold.ttf', 15)
+            self.big_font=pygame.font.Font('freesansbold.ttf', 24)
+            self.heading=self.big_font.render("Highscores", True, (85,69,15))
+            self.text = []
+            self.display=[]
+            self.rects=[]
+            scores_file=open("scores.txt","r")
+            self.back_rect=pygame.Rect(275,444,146,50)
+            for line in scores_file:
+                current_line=line.split(",")
+                self.text+=[current_line[0]]
+                self.text+=[current_line[1][:-1]]
+                self.text[len(self.text)-1]=int(self.text[len(self.text)-1])
+                
+            for i in range(1,len(self.text)-2,2):
+                for j in range(i, len(self.text),2):
+                    print(self.text[i])
+                    print(self.text[j])
+                    if self.text[i]<self.text[j]:
+                        temp=self.text[i]
+                        self.text[i]=self.text[j]
+                        self.text[j]=temp
+                        temp=self.text[i-1]
+                        self.text[i-1]=self.text[j-1]
+                        self.text[j-1]=temp
+            self.num_scores=len(self.text)
+            if self.num_scores>20:self.num_scores=20
+            for i in range(self.num_scores):
+                temp=self.small_font.render(str(self.text[i]), True, (85,69,15))
+                self.display+=[temp]
+                if i%2==0:
+                    self.rects+=[temp.get_rect(topleft=(122,100+i*15))]
+                else:
+                    self.rects+=[temp.get_rect(topright=(575,100+(i-1)*15))]
+                    
+       
+            #Game Clock & Speed
+            self.clock = pygame.time.Clock()
+            self.fps = 60
+
+
+        def draw(self):
+            self.screen.fill((0, 0, 255))
+            self.screen.blit(self.bg, (0, 0))
+            self.screen.blit(self.heading,(282,70))
+            for i in range(self.num_scores):
+                self.screen.blit(self.display[i],self.rects[i])
+            pygame.display.flip()
+            self.clock.tick(60)
+
+        def help_loop(self):
+             while True:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        quit()
+                    if event.type==pygame.MOUSEBUTTONUP:
+                        pos=pygame.mouse.get_pos()
+                        if self.back_rect.collidepoint(pos):
+                            instance=Menu()
+                            instance.menu_loop()
+
+                self.draw()
+'''
+
+
+
+
+
